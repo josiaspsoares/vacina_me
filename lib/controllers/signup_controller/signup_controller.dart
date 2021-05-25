63f9343a3ddea66d271c7af5c01364dd98ca110a
josiassoares <61014395+josiassoares@users.noreply.github.com>
2021-05-25 15:15:06 -0300

    Applying state management with Provider

--- lib/controllers/signup_controller/signup_controller.dart
+++ lib/controllers/signup_controller/signup_controller.dart
@@ -2,14 +2,29 @@ import 'package:flutter/material.dart';
 import '../../repositories/account_repository/account_repository.dart';
 import '../../viewmodel/signup_viewmodel/signup_viewmodel.dart';
 
+enum SignupState { idle, success, error, loading }
+
 class SignupController extends ChangeNotifier {
   AccountRepository? repository;
+  var state = SignupState.idle;
+
   SignupController() {
     repository = AccountRepository();
   }
 
   Future<String?> create(SignupViewModel model) async {
-    var message = await repository!.createAccount(model);
-    return message;
+    state = SignupState.loading;
+    notifyListeners();
+
+    try {
+      var message = await repository!.createAccount(model);
+      state = SignupState.success;
+      notifyListeners();
+      return message;
+    } catch (e) {
+      state = SignupState.error;
+      notifyListeners();
+      throw e;
+    }
   }
 }
--- lib/controllers/vaccination_data_search_controller/vaccination_data_search_controller.dart
+++ lib/controllers/vaccination_data_search_controller/vaccination_data_search_controller.dart
@@ -1,16 +1,32 @@
+import 'package:flutter/foundation.dart';
 import 'package:vacina_me/models/user_model/user_model.dart';
 import 'package:vacina_me/repositories/account_repository/account_repository.dart';
 import 'package:vacina_me/viewmodel/vaccination_data_search_viewmodel/vaccination_data_search_viewmodel.dart';
 
-class VaccinationDataSearchController {
+enum VaccinationDataSearchState { idle, success, error, loading }
+class VaccinationDataSearchController extends ChangeNotifier{
   AccountRepository? repository;
+  var state = VaccinationDataSearchState.idle;
 
   VaccinationDataSearchController() {
     repository = AccountRepository();
   }
 
   Future<UserModel?> search(VaccinationDataSearchViewModel model) async {
-    var user = await repository!.readAccountData(model);
-    return user;
+    state =  VaccinationDataSearchState.loading;
+    notifyListeners();
+
+    try {
+      var user = await repository!.readAccountData(model);
+      state = VaccinationDataSearchState.success;
+      notifyListeners();
+      return user;
+    } catch (e) {
+      state = VaccinationDataSearchState.error;
+      notifyListeners();
+      throw e;
+    }
+
+    
   }
 }
