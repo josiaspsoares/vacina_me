f67f09b2dc1458adad59ba0bd641ce8a25320d37
josiassoares <61014395+josiassoares@users.noreply.github.com>
2021-06-19 19:03:46 -0300

    updating homeSubtitle

--- lib/screens/home_screen/components/home_texts.dart
+++ lib/screens/home_screen/components/home_texts.dart
@@ -1,3 +1,3 @@
 String homeTitle = 'Vacina-me';
 String homeSubtitle =
-    'Nossa startup desenvolve o software VacinaMe, uma aplicação para controlar processos de vacinação e fazer gestão de pessoas vacinadas. Isso lhe proporcionará uma melhor gestão da vacinação, bem como o controle em relação às doses e à quantidade de pessoas vacinadas com  mais eficiência e de forma digital.';
+    'Nossa startup desenvolve o software VacinaMe, uma aplicação em linguagem C para controlar processos de vacinação e fazer gestão de pessoas vacinadas. Isso lhe proporcionará uma melhor gestão da vacinação, bem como o controle em relação às doses e à quantidade de pessoas vacinadas com  mais eficiência e de forma digital.';
